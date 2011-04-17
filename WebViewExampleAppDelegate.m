26ab88ecb9298dfd2011f8e42af0a580fc99193c
Matt Hackett <richtaur@gmail.com>
2011-04-17 14:46:36 -0700

    adding steps for the Completing your native Mac OSX App built in HTML5 article: http://blog.lostdecadegames.com/completing-your-native-mac-osx-app-built-in-h

--- WebViewExampleAppDelegate.m
+++ WebViewExampleAppDelegate.m
@@ -14,9 +14,18 @@
 }
 
 - (void)awakeFromNib {
+    WebPreferences *prefs = [webView preferences];
+	[prefs _setLocalStorageDatabasePath:@"~/Library/WebViewExample/LocalStorage"];
+    
 	NSString *resourcesPath = [[NSBundle mainBundle] resourcePath];
 	NSString *htmlPath = [resourcesPath stringByAppendingString:@"/htdocs/640x480_naked.html"];
 	[[webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:htmlPath]]];
+    
+    [window setDelegate:self];
+}
+
+- (void)windowWillClose:(NSNotification *)aNotification {
+	[NSApp terminate:self];
 }
 
 @end
